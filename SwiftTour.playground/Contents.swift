ebc85055f77c1eef04d27c1858543c23d47a776e
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-01-28 23:22:37 +0900

    Study Control Flow

--- SwiftTour.playground/Contents.swift
+++ SwiftTour.playground/Contents.swift
@@ -23,11 +23,15 @@
 ---
 */
 
+
+
 // 2025/01/23 Kazutoshi Nagahashi
 // 「Swift Tour」専用
 import UIKit
 
+/* 下記コードのため一旦コメントアウト
 var greeting = "Hello, playground"
+*/
 
 // "Hello world!"
 print("Hello World!")
@@ -65,6 +69,8 @@ let quotation = """
     I still have \(apples + oranges) pieces of fruit.
 """
 
+
+
 // 2025/01/25 Kazutoshi Nagahashi
 // **クロージャ（Closure）**とは、特定の処理をまとめたコードのかたまりで、名前を持たない関数（無名関数）の一種です。
 // Swiftでは、高度な処理やコードの簡略化のために頻繁に使われます。
@@ -94,7 +100,8 @@ print(message) // "Hello, Swift!"
 // "Hello, \(name)!": 処理内容（戻り値）。
 
 
-// 20250126
+
+// 2025/01/26 Kazutoshi Nagahashi
 // ■配列や辞書の作成
 // 角括弧([])を使い、それらの要素へアクセスする際は角括弧内にインデックスやキーを書きます。
 // 最後の要素の後ろにもカンマ(,)を付けることができます。
@@ -129,8 +136,6 @@ occupations = [:]
 let emptyArray: [String] = []
 let emptyDictionary: [String: Float] = [:]
 
-
-
 // ■制御フロー（Control Flow）
 // 条件分岐を作成するには、if と switch を使います。
 // ループを作成するには for-in while repeat-whileを使います。
@@ -139,6 +144,7 @@ let emptyDictionary: [String: Float] = [:]
 
 // 例：このコードは、配列 individualScores に含まれる個々のスコアをループでチェックし、
 // 条件に応じて teamScore に点数を加算しています。
+
 let individualScores = [75, 43, 103, 87, 12]
 var teamScore = 0
 for score in individualScores {
@@ -153,3 +159,104 @@ print(teamScore)
 
 
 
+// 2025/01/28 Kazutoshi Nagahashi
+/*　？なぜエラーになるのか質問した方がいいかも？
+let scoreDecoration = if teamScore > 10 {
+    "🎉"
+} else {
+    ""
+}
+print("Score:", teamScore, scoreDecoration)
+// "Score: 11 🎉"を出力
+*/
+
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+// false
+
+// オプショナル値が nil の場合、条件は false になり、中括弧内のコードはスキップされます。
+// nil でなければ、オプショナル値はアンラップされて let の後の定数に代入され、中括弧内のブロック内でその値を利用できます。
+var optionalName: String? = "John Appleseed"
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
+
+// オプショナル値を扱うもう 1 つの方法として、?? 演算子を使用してデフォルトの値を提供する方法があります。
+// もしオプショナル値が nil の場合、代わりにデフォルトの値が使われます。
+// ？基本的な解説が必要かも？
+let nickName: String? = nil
+let fullName: String = "John Appleseed"
+let informalGreeting = "Hi \(nickName ?? fullName)"
+
+// アンラップ後の変数に同名を用いる場合、右辺を省略することができます。
+// ？なぜifと変数の間にletが入るの？
+if let nickName {
+    print("Hey, \(nickName)")
+}
+
+// switchは、あらゆる種類のデータと比較のための演算子を扱うことができます。
+// (整数や等価チェックだけに限定されません)
+// ？まずswitch-case文の基本を教えてくれよ？
+// ↓何やってんだよ？
+// case let x where x.hasSuffix("pepper"):
+// print("Is it a spicy \(x)?")
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants on a log.")
+case "cucumber", "watercress":
+    print("That would make a good tea sandwich.")
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)?")
+default:
+    print("Everything tastes good in soup.")
+}
+// Is it a spicy red pepper?
+
+
+
+// for-in を使用することで辞書のそれぞれの要素のキーバリューペアを受け取って、辞書内のアイテムに反復処理をすることができます。
+// 辞書は順序のないコレクションなので、受け取るキーバリューペアの順序は決まっていません。
+// ？何だよこのコード、詳細解説よろしく？
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+for (_, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print(largest)
+// 25
+
+// while を使用することで、条件が変わるまでブロック内のコードを反復して実行できます。
+// ループの条件を最後に置くことで、ループ内のブロックが少なくとも 1 回実行されるようにすることができます。
+// ？repeatって何やねん？
+// ？コードについて教えて？
+var n = 2
+while n < 100 {
+    n *= 2
+}
+print(n)
+// 128
+
+var m = 2
+repeat {
+    m *= 2
+} while m < 100
+print(m)
+// 128
+
+// ..< を使用するとインデックスの範囲を生成でき、ループのインデックスを追うことができます。
+var total = 0
+for i in 0..<4 {
+    total += i
+}
+print(total)
+// 6
